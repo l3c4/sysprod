73173fad562575f42b7465b7bde55573239c89f4
L3C4 <leandro_estima@hotmail.com>
2020-09-08 21:28:09 -0300

    First commit

---
+++ src/main/java/br/com/estima/sysprod/SysprodApplication.java
@@ -0,0 +1,13 @@
+package br.com.estima.sysprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysprodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysprodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estima/sysprod/SysprodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estima.sysprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysprodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
